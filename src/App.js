d158be45cb74dcc6e6080a50c061a539b58d6ae5
zmeyer44 <meyerz@wustl.edu>
2020-06-17 16:58:08 -0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ActivityList from './components/ActivityList';
+import Details from './components/Details';
+import Default from './components/Default';
+import Home from './components/Home';
+import VirtualFieldtrip from './components/VirtualFieldtrip';
+import Calender from './components/Calender';
+import Reading from './components/Reading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ScrollToTop from "./ScrollToTop";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/activities" component={ActivityList} />
+          <Route path="/details" component={Details} />
+          <Route path="/vft" component={VirtualFieldtrip} />
+          <Route path="/calender" component={Calender} />
+          <Route path="/reading" component={Reading} />
+          <Route path=" vft" component={VirtualFieldtrip} />
+          <Route component={Default} />
+        </Switch>
+        <ScrollToTop />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
